9fbd0c226da9f402f3ff0396a63a763137749d00
Babmuclr <it.onepiece.balsreal@gmail.com>
2021-06-26 22:06:49 +0900

    scafford

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'icon_content.dart';
+import 'reusable_card.dart';
+import 'constants.dart';
 
 void main() => runApp(BMICalculator());
+
+enum Gender{
+  male,
+  female,
+}
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: InputPage(),
+      theme: ThemeData.dark(),
     );
   }
 }
@@ -17,15 +28,128 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+
+  Gender choiceGender = Gender.male;
+  int height = 180;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    color: choiceGender == Gender.male
+                    ? kInactiveCardColor
+                    : kActiveCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.mars,
+                      label: 'MALE',
+                    ),
+                    onPress: () => {},
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: choiceGender == Gender.female
+                    ? kInactiveCardColor
+                    : kActiveCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: 'MALE',
+                    ),
+                    onPress: () => {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              color: kInactiveCardColor,
+              cardChild: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text('HEIGHT'),
+                      Text('cm'),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                      activeTrackColor: Colors.white,
+                      thumbColor: Color(0xFFEB1555),
+                      overlayColor: Color(0x29EB1555),
+                      thumbShape:
+                      RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                      RoundSliderOverlayShape(overlayRadius: 30.0),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      min: 120.0,
+                      max: 220.0,
+                      onChanged: (double newValue) {
+                        setState(() {
+                          height = newValue.round();
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              onPress: () => {},
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: ReusableCard(
+                    color: kInactiveCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.instagram,
+                      label: 'WEIGHT',
+                    ),
+                    onPress: () => {},
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    color: kInactiveCardColor,
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.instagram,
+                      label: 'WEIGHT',
+                    ),
+                    onPress: () => {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            color: kBottomContainerColor,
+            margin: EdgeInsets.only(top: 10.0),
+            height: 80.0,
+            child: Center(
+              child: Text(
+                'CALCULATE',
+                style: TextStyle(
+                  fontSize: 40.0,
+                ),
+              ),
+            ),
+          ),
+        ],),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
